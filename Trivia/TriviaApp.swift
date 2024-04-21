4ade82095928201e85037140bf08bef9e362e2e3
Jasper Albers <jasperalbers@posteo.de>
2024-04-21 16:16:41 +0200

    Initial Commit

---
+++ Trivia/TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaApp.swift
+//  Trivia
+//
+//  Created by Jasper on 21.04.24.
+//
+
+import SwiftUI
+
+@main
+struct TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
